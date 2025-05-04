6c4d1c2902a14a77349b489b1f61fe1501f2bac1
Frank Smith <fasvi6@gmail.com>
2025-05-03 20:31:26 -0400

    Use glitch version

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 
-export let socket = io('http://localhost:4000', {autoConnect: false})
+// export let socket = io('http://localhost:4000', {autoConnect: false})
+export let socket = io('https://munchkin-companion-server.glitch.me', {autoConnect: false})
 
