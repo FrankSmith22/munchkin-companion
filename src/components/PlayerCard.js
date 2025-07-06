e57d242b49750e06ff00904fe0a1bb0f2fb0232d
Frank Smith <fasvi6@gmail.com>
2025-07-06 00:52:13 -0400

    Add collapsible sidebar with mock party data

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,18 +1,62 @@
+import { Button, Container, Row, Col } from 'reactstrap';
 import { EVENTS as E } from '../app/events.mjs';
 import BackButton from './BackButton';
+import '../sidebar.css';
+import { useState } from 'react';
 
 export default function PlayerCard({socket, playerObj}){
 
+    const [sidebarToggle, setSidebarToggle] = useState(true)
+    const [sidebarWidth, setSidebarWidth] = useState("100px")
+    const [sidebarBtnPosition, setSidebarBtnPosition] = useState("75px")
+    const [sidebarBtnRotate, setSidebarBtnRotate] = useState("0deg")
+
+    const toggleSidebar = () => {
+        setSidebarToggle(!sidebarToggle)
+        if (sidebarToggle){
+            setSidebarWidth("100px")
+            setSidebarBtnPosition("75px")
+            setSidebarBtnRotate("0deg")
+        }
+        else{
+            setSidebarWidth("0px")
+            setSidebarBtnPosition("20px")
+            setSidebarBtnRotate("180deg")
+        }
+    }
+
     return (
-        <div>
-            <BackButton socket={socket} confirm={true}/>
-            Name: {playerObj.name}
-            <br></br><br></br>
-            level: {playerObj.level} <button onClick={e => socket.emit(E.PLAYER_LEVEL_INC)}>+</button> <button onClick={e => socket.emit(E.PLAYER_LEVEL_DEC)}>-</button>
-            <br></br><br></br>
-            gear bonus: {playerObj.gearBonus} <button onClick={e => socket.emit(E.PLAYER_GEAR_INC)}>+</button> <button onClick={e => socket.emit(E.PLAYER_GEAR_DEC)}>-</button>
-            <br></br><br></br>
-            <b>total: {playerObj.level + playerObj.gearBonus}</b>
+        <>
+        <button className='sidebarToggleBtn' style={{ left: sidebarBtnPosition, rotate: sidebarBtnRotate }} onClick={e => toggleSidebar()}>{"<"}</button>
+        <div id="mySidebar" class="sidebar" style={{ width: sidebarWidth }}>
+            <ul>
+                <li>
+                Name:  Elegos<br/>
+                Level: 5<br/>
+                Total: 16<br/><br/>
+                </li>
+                <li>
+                Name:  Mononoke<br/>
+                Level: 3<br/>
+                Total: 20<br/>
+                </li>
+            </ul>
         </div>
+        {/* Main contents */}
+        <Container style={{ height: "100%" }}>
+            <Row style={{ height: "100%" }}>
+                <Col className="offset-2">
+                    <BackButton socket={socket} confirm={true}/>
+                    <span style={{ fontSize: "36px" }}>{playerObj.name}</span>
+                    <br></br><br></br>
+                    level: {playerObj.level} <Button className="playerButton" onClick={e => socket.emit(E.PLAYER_LEVEL_INC)}>+</Button> <Button className="playerButton" onClick={e => socket.emit(E.PLAYER_LEVEL_DEC)}>-</Button>
+                    <br></br><br></br>
+                    gear bonus: {playerObj.gearBonus} <Button className="playerButton" onClick={e => socket.emit(E.PLAYER_GEAR_INC)}>+</Button> <Button className="playerButton" onClick={e => socket.emit(E.PLAYER_GEAR_DEC)}>-</Button>
+                    <br></br><br></br>
+                    <b>total: {playerObj.level + playerObj.gearBonus}</b>
+                </Col>
+            </Row>
+        </Container>
+        </>
     )
 }
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // dev
-export let socket = io('http://192.168.1.169:4000', {autoConnect: false})
+// export let socket = io('http://192.168.1.169:4000', {autoConnect: false})
 // live
-// export let socket = io('https://munchkin-companion-server.glitch.me', {autoConnect: false})
+export let socket = io('https://munchkin-companion-server.glitch.me', {autoConnect: false})
 
