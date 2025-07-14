9852c8643aed17ef590d94f6d15d1f2712f9f2f2
Frank Smith <fasvi6@gmail.com>
2025-07-13 23:34:15 -0400

    Hosting on render.com!

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // dev
-export let socket = io('http://192.168.1.169:4000', {autoConnect: false})
+// export let socket = io('http://192.168.1.169:4000', {autoConnect: false})
 // live
 // TODO find a new hosting platform, RIP glitch hosting
-// export let socket = io('https://munchkin-companion-server.glitch.me', {autoConnect: false})
+export let socket = io('https://munchkin-companion-server.onrender.com', {autoConnect: false})
 
