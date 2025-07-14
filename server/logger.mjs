57aee0679d9ee99c8f77c278d6c0bed3c75e7b4b
Frank Smith <fasvi6@gmail.com>
2025-07-13 23:30:26 -0400

    Dont write logs to file for now

--- server/logger.mjs
+++ server/logger.mjs
@@ -6,16 +6,16 @@ export const logger = createLogger({
   level: LOG_LEVEL,
   format: format.json(),
   defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or higher to `error.log`
-    //   (i.e., error, fatal, but not other levels)
-    //
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    //
-    // - Write all logs with importance level of `info` or higher to `combined.log`
-    //   (i.e., fatal, error, warn, and info, but not trace)
-    //
-    new transports.File({ filename: 'combined.log' }),
-  ],
+  // transports: [
+  //   //
+  //   // - Write all logs with importance level of `error` or higher to `error.log`
+  //   //   (i.e., error, fatal, but not other levels)
+  //   //
+  //   new transports.File({ filename: 'error.log', level: 'error' }),
+  //   //
+  //   // - Write all logs with importance level of `info` or higher to `combined.log`
+  //   //   (i.e., fatal, error, warn, and info, but not trace)
+  //   //
+  //   new transports.File({ filename: 'combined.log' }),
+  // ],
 });
