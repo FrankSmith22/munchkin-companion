e0c8d10fb0d667a8affdefb013d971860518f3fb
FrankSmith22 <fasvi6@gmail.com>
2025-12-13 01:09:52 -0500

    progress

--- src/components/CardCreator.js
+++ src/components/CardCreator.js
@@ -1,21 +1,80 @@
 import BackButton from "./BackButton";
-import { FormSelect } from "react-bootstrap";
-import { Container, Row, Col, Button } from "reactstrap";
+import { Row, Col, CardDeck } from "reactstrap";
 import Modal from 'react-bootstrap/Modal';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Draggable from "react-draggable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins, faDoorClosed, faGrip, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 export default function CardCreator({socket, setDisplayMode, isConnected, setShowDisconnectedToast}){
 
+    const CARD_TYPES = {
+        DOOR: "door",
+        TREASURE: "treasure"
+    }
+
     const [newCardModalIsOpen, setNewCardModalIsOpen] = useState(false)
-    const [cardType, setCardType] = useState("")
+    const [cardType, setCardType] = useState(CARD_TYPES.DOOR)
 
     const toggleNewCardModalIsOpen = () => setNewCardModalIsOpen(!newCardModalIsOpen)
 
+    
     const cardSetupModal = () => {
+        const toggleCardType = () => {
+            if (cardType === CARD_TYPES.DOOR){
+                setCardType(CARD_TYPES.TREASURE)
+            } else {
+                setCardType(CARD_TYPES.DOOR)
+            }
+        }
+        const modalBodyClasses = "mx-auto mt-4 mt-md-0 draggableParent "
         return (
+            // TODO maybe implement this proper one day... Just overkill for now
+            // <Modal show={newCardModalIsOpen} onHide={toggleNewCardModalIsOpen} className="munchkinModal newCardCreatorModal">
+            //     <Modal.Body
+            //         className={modalBodyClasses + (cardType === CARD_TYPES.DOOR ? "doorCardColor" : "treasureCardColor")}
+            //     >
+            //         <FontAwesomeIcon
+            //             icon={cardType === "door" ? faDoorClosed : faCoins}
+            //             onClick={toggleCardType}
+            //         />
+            //         <Draggable
+            //             bounds=".draggableParent"
+            //             handle=".handle"
+            //         >
+            //             <div style={{display: "inline-block"}} className="text-center">
+            //                 <div className="d-flex justify-content-evenly">
+            //                     <FontAwesomeIcon 
+            //                         className="handle"
+            //                         icon={faGrip}
+            //                     />
+            //                     <FontAwesomeIcon
+            //                         icon={faTrashCan}
+            //                     />
+            //                 </div>
+            //                 <span contentEditable>hello world</span>
+            //             </div>
+            //         </Draggable>
+            //     </Modal.Body>
+            // </Modal>
             <Modal show={newCardModalIsOpen} onHide={toggleNewCardModalIsOpen} className="munchkinModal newCardCreatorModal">
-                <Modal.Body className="mx-auto mt-4 mt-md-0">
-                    <div draggable>hello world</div>
+                <Modal.Body className={modalBodyClasses + (cardType === CARD_TYPES.DOOR ? "doorCardColor" : "treasureCardColor")}>
+                    <FontAwesomeIcon
+                        icon={cardType === "door" ? faDoorClosed : faCoins}
+                        onClick={toggleCardType}
+                        style={{position: "absolute", height: "2rem"}}
+                    />
+                    <div contentEditable className="text-center mx-auto" style={{fontSize: "1rem", width: "90%"}}>level/bonus</div>
+                    <div contentEditable className="text-center" style={{fontSize: "2rem"}}>Title</div>
+                    <div contentEditable className="text-center" style={{fontSize: "1rem"}}>Subtitle</div>
+                    {/* Need:
+                        1. level/bonus
+                        2. title
+                        3. subtitle
+                        4. image
+                        5. description
+                        6. footer (armor | levels           gp value | treasure)
+                    */}
                 </Modal.Body>
             </Modal>
         )
